d1d4d21b91f15f063c1f29d2502e676a84db372a
fish-puce <xfsarzosa@puce.edu.ec>
2023-11-23 09:56:39 -0500

    commit inicial

---
+++ login.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle login logic here
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
